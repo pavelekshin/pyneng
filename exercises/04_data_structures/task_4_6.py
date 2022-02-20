d7e45ebf048334bb7c687dd34e91127e3ab8c709
Pavel Ekshin <pavel.ekshine@gmail.com>
2022-02-20 11:00:27 -0600

    Сделаны задания 4_6

--- exercises/04_data_structures/task_4_6.py
+++ exercises/04_data_structures/task_4_6.py
@@ -18,3 +18,14 @@ Outbound Interface    FastEthernet0/0
 """
 
 ospf_route = "      10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
+
+prefix,metric,via,nh,last_upd,out_int = ospf_route.strip().replace(",","").split()
+metric = metric.strip("][")
+template = """
+Prefix              {}
+AD/Metric           {}
+Next-Hop            {}
+Last update         {}
+Outbound Interface  {}
+"""
+print(template.format(prefix,metric,nh,last_upd,out_int))
