7872f15c4a5d43992199df327dbf13266ba3e820
Pavel Ekshin <pavel.ekshine@gmail.com>
2022-03-19 10:15:24 -0500

    Сделаны задания 9_3a

--- exercises/09_functions/task_9_3a.py
+++ exercises/09_functions/task_9_3a.py
@@ -25,3 +25,28 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+
+def get_int_vlan_map(config_filename):
+    access_dict = {}
+    trunk_dict = {}
+    with open(config_filename, "r") as f:
+        for line in f:
+            if "FastEthernet" in line:
+                intf = line.split()[-1]
+            elif "trunk allowed vlan" in line:
+                _vlan = line.split()[-1].split(",")
+                _vlan_list = sorted({int(item) for item in _vlan})
+                trunk_dict[intf] = _vlan_list
+            elif "access vlan" in line:
+                _vlan = int(line.split()[-1])
+                access_dict[intf] = _vlan
+            elif "mode access" in line:
+                if intf not in access_dict.keys() or trunk_dict.keys():
+                    access_dict[intf] = 1
+        return access_dict, trunk_dict
+
+
+access, trunk = get_int_vlan_map("config_sw2.txt")
+print(access)
+print(trunk)
