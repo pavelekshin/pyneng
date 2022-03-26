0436a0d0be53337c9c470e83df78e684376eeb4d
Pavel Ekshin <pavel.ekshine@gmail.com>
2022-03-26 03:35:41 -0500

    Сделаны задания 11_2

--- exercises/11_modules/task_11_2.py
+++ exercises/11_modules/task_11_2.py
@@ -28,9 +28,36 @@ Cгенерировать топологию, которая соответст�
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+
+from pprint import pprint
+
+from task_11_1 import parse_cdp_neighbors
+
 infiles = [
     "sh_cdp_n_sw1.txt",
     "sh_cdp_n_r1.txt",
     "sh_cdp_n_r2.txt",
     "sh_cdp_n_r3.txt",
 ]
+
+
+#def sorted_dict(indict):
+#    result = {key: value for key, value in sorted(indict.items())}
+#    return result
+
+
+def create_network_map(filenames):
+    """
+    Network map function
+    """
+    network_map = {}
+    for file in infiles:
+        with open(file, "r") as f:
+            network_map.update(parse_cdp_neighbors(f.read()))
+#    result = (sorted_dict(network_map))
+#    pprint(network_map)
+    return network_map
+
+
+if __name__ == "__main__":
+    create_network_map(infiles)
