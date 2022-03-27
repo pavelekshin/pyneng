063b976a6d24b6f31604b25d8b6085db2b7c256d
Pavel Ekshin <pavel.ekshine@gmail.com>
2022-03-27 06:04:30 -0500

    Сделаны задания 12_2

--- exercises/12_useful_modules/task_12_2.py
+++ exercises/12_useful_modules/task_12_2.py
@@ -34,3 +34,51 @@
  '172.21.41.129', '172.21.41.130', '172.21.41.131', '172.21.41.132']
 
 """
+
+from pprint import pprint
+
+import ipaddress
+
+ipaddrlist = ["10.1.1.1", "10.4.10.10-13", "192.168.1.12-192.168.1.15"]
+
+
+def check_ip(ip):
+    try:
+        ipaddress.ip_address(ip)
+        return True
+    except ValueError:
+        return False
+
+
+def convert_ranges_to_ip_list(ip_address):
+    """
+    convert ip:
+    10.1.1.1
+    10.1.1.1-10.1.1.10
+    10.1.1.1-10
+    """
+    iplist = []
+    for ip in ip_address:
+        if check_ip(ip):
+            iplist.append(ip)
+        else:
+            ip, iprange = ip.split("-")
+            if 1 <= len(iprange) <= 3 and check_ip(ip):
+                _start = int(ip.split(".")[-1])
+                _end = int(iprange)
+                _range = (_end - _start) +1
+                print(_range)
+                ip = ipaddress.ip_address(ip)
+                _iplist = [iplist.append(str(ip + x)) for x in range(_range)]
+            elif len(iprange) > 3 and check_ip(ip):
+                _start = int(ip.split(".")[-1])
+                _end = int(iprange.split(".")[-1])
+                _range = (_end - _start) +1
+                ip = ipaddress.ip_address(ip)
+                _iplist = [iplist.append(str(ip + x)) for x in range(_range)]
+    return iplist
+
+
+if __name__ == "__main__":
+    result = convert_ranges_to_ip_list(ipaddrlist)
+    pprint(result)
