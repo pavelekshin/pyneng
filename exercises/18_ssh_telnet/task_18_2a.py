c16c4d4a6db65f5fad1a5c6b4c64094aff3b7756
Pavel Ekshin <pavel.ekshine@gmail.com>
2022-05-22 13:05:42 -0500

    Сделаны задания 18_2a

--- exercises/18_ssh_telnet/task_18_2a.py
+++ exercises/18_ssh_telnet/task_18_2a.py
@@ -19,3 +19,33 @@ In [15]:
 Скрипт должен отправлять список команд commands на все устройства
 из файла devices.yaml с помощью функции send_config_commands.
 """
+
+
+from pprint import pprint
+import yaml
+from netmiko import (
+    ConnectHandler,
+    NetmikoTimeoutException,
+    NetmikoAuthenticationException,
+)
+
+
+def send_config_commands(device, config_commands, log=True):
+    try:
+        if log:
+            print(f"""Подключаюсь к {device["host"]}...""")
+        with ConnectHandler(**device) as ssh:
+            ssh.enable()
+            result = ssh.send_config_set(config_commands)
+        return result
+    except (NetmikoTimeoutException, NetmikoAuthenticationException) as error:
+        print(error)
+
+
+if __name__ == "__main__":
+    commands = ["logging 10.255.255.1", "logging buffered 20010", "no logging console"]
+    with open("devices.yaml") as f:
+        devices = yaml.safe_load(f)
+
+    for dev in devices:
+        print(send_config_commands(dev, commands,log=False))
