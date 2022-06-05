b2375be4d8cc6e90fe2b7f244300de6d51bd367d
Pavel Ekshin <pavel.ekshine@gmail.com>
2022-06-05 10:12:20 -0500

    Сделаны задания 19_2

--- exercises/19_concurrent_connections/task_19_2.py
+++ exercises/19_concurrent_connections/task_19_2.py
@@ -34,3 +34,46 @@ Ethernet0/1                unassigned      YES NVRAM  administratively down down
 
 Проверить работу функции на устройствах из файла devices.yaml
 """
+
+from pprint import pprint
+import yaml
+from netmiko import (
+    ConnectHandler,
+    NetmikoTimeoutException,
+    NetmikoAuthenticationException,
+)
+from concurrent.futures import ThreadPoolExecutor
+
+
+def send_show_command(device, command):
+    try:
+        with ConnectHandler(**device) as ssh:
+            ssh.enable()
+            result = ssh.send_command(command)
+            router = ssh.find_prompt()
+        return {router : result}
+    except (NetmikoTimeoutException, NetmikoAuthenticationException) as error:
+        print(error)
+
+
+def send_show_command_to_devices(devices, command, filename, limit=3):
+    with ThreadPoolExecutor(max_workers=limit) as executor:
+        future_list = []
+        for device in devices:
+            future = executor.submit(send_show_command, device, command)
+            future_list.append(future)
+    with open(filename, "w") as w:
+        for f in future_list:
+            result = f.result()
+            for router, value in result.items():
+                w.write(f"{router}{command}" + "\n")
+                w.write(f"{value}" + "\n")
+    return None
+
+
+if __name__ == "__main__":
+    command = "sh ip int br"
+    with open("devices.yaml") as f:
+        devices = yaml.safe_load(f)
+
+    print(send_show_command_to_devices(devices, command, filename="out.txt"))
