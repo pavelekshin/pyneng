55efdcc550792301a774d31be814e55b74cde7ab
Pavel Ekshin <pavel.ekshine@gmail.com>
2022-06-21 09:24:08 -0500

    Сделаны задания 20_2

--- exercises/20_jinja2/task_20_2.py
+++ exercises/20_jinja2/task_20_2.py
@@ -17,3 +17,29 @@
 В качестве данных, используйте информацию из файла data_files/router_info.yml
 
 """
+import os
+import glob
+
+import yaml
+
+from task_20_1 import generate_config
+
+
+def generate_template(template_dir, data, *, filename=None):
+    templates = glob.glob(template_dir + "[!f]*.txt")
+    templates.reverse()
+    new_template = f"{template_dir}{filename}"
+    for template in templates:
+        with open(template, "r") as f, open(new_template, "a") as w:
+            file = f.read()
+            w.write(file)
+            w.write("\n")
+    print(generate_config(new_template, data))
+
+
+if __name__ == "__main__":
+    template_dir = "templates/"
+    data_file = "data_files/router_info.yml"
+    with open(data_file) as f:
+        data = yaml.safe_load(f)
+    generate_template(template_dir, data, filename="cisco_router_base.txt")
