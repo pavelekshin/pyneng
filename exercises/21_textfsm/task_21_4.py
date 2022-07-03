e3af88fe95cecea093d074f4771908e8f88784af
Pavel Ekshin <pavel.ekshine@gmail.com>
2022-07-03 14:44:58 -0500

    Сделаны задания 21_4

--- exercises/21_textfsm/task_21_4.py
+++ exercises/21_textfsm/task_21_4.py
@@ -21,3 +21,61 @@
 Проверить работу функции на примере вывода команды sh ip int br
 и устройствах из devices.yaml.
 """
+
+from textfsm import clitable
+from pprint import pprint
+import yaml
+import re
+from netmiko import (
+    ConnectHandler,
+    NetmikoTimeoutException,
+    NetmikoAuthenticationException,
+)
+from concurrent.futures import ThreadPoolExecutor
+
+
+def send_show_command(device, command):
+    try:
+        with ConnectHandler(**device) as ssh:
+            ssh.enable()
+            result = ssh.send_command(command)
+            router = ssh.find_prompt()
+        return result
+    except (NetmikoTimeoutException, NetmikoAuthenticationException) as error:
+        print(error)
+
+
+def send_show_command_to_devices(devices, *, command=None, limit=3):
+    with ThreadPoolExecutor(max_workers=limit) as executor:
+        future_list = []
+        for device in devices:
+            future = executor.submit(send_show_command, device, command)
+            future_list.append(future)
+    output = ""
+    for out in future_list:
+        output += out.result()
+    return output
+
+
+def send_and_parse_show_command(device_dict, command, templates_path, index="index"):
+    if type(device_dict) is list:
+        command_output = send_show_command_to_devices(device_dict, command=command)
+    else:
+        command_output = send_show_command(device_dict, command)
+    cli_table = clitable.CliTable(index, templates_path)
+    cli_table.ParseCmd(
+        command_output, {"Command": command}
+    )
+    data_rows = [list(row) for row in cli_table]
+    header = list(cli_table.header)
+    return [dict(zip(header, line)) for line in data_rows]
+
+
+if __name__ == "__main__":
+    with open("devices.yaml") as f:
+        devices = yaml.safe_load(f)
+    print(
+        send_and_parse_show_command(
+            devices, "sh ip int br", templates_path="templates", index="index"
+        )
+    )
