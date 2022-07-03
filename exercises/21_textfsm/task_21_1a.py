0dc79ee0ce926a2192bceae89e03f65c70d97fd8
Pavel Ekshin <pavel.ekshine@gmail.com>
2022-07-03 03:34:35 -0500

    Сделаны задания 21_1a

--- exercises/21_textfsm/task_21_1a.py
+++ exercises/21_textfsm/task_21_1a.py
@@ -16,3 +16,31 @@
 Проверить работу функции на выводе команды output/sh_ip_int_br.txt
 и шаблоне templates/sh_ip_int_br.template.
 """
+
+
+from netmiko import ConnectHandler
+import textfsm
+
+
+def parse_output_to_dict(template, command_output):
+    if command_output and template:
+        with open(template, "r") as template:
+            fsm = textfsm.TextFSM(template)
+            result = fsm.ParseTextToDicts(command_output)
+    return result
+
+
+# вызов функции должен выглядеть так
+if __name__ == "__main__":
+    r1_params = {
+        "device_type": "cisco_ios",
+        "host": "192.168.100.1",
+        "username": "cisco",
+        "password": "cisco",
+        "secret": "cisco",
+    }
+    with ConnectHandler(**r1_params) as r1:
+        r1.enable()
+        output = r1.send_command("sh ip int br")
+    result = parse_output_to_dict("templates/sh_ip_int_br.template", output)
+    print(result)
