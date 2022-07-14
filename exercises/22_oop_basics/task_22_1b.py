d57b7848f83d3aa874345fa42cf4861ca5d9e930
Pavel Ekshin <pavel.ekshine@gmail.com>
2022-07-14 13:37:52 -0500

    Сделаны задания 22_1b

--- exercises/22_oop_basics/task_22_1b.py
+++ exercises/22_oop_basics/task_22_1b.py
@@ -52,14 +52,49 @@ In [13]: t.delete_link(('R5', 'Eth0/0'), ('R3', 'Eth0/2'))
 
 """
 
-topology_example = {
-    ("R1", "Eth0/0"): ("SW1", "Eth0/1"),
-    ("R2", "Eth0/0"): ("SW1", "Eth0/2"),
-    ("R2", "Eth0/1"): ("SW2", "Eth0/11"),
-    ("R3", "Eth0/0"): ("SW1", "Eth0/3"),
-    ("R3", "Eth0/1"): ("R4", "Eth0/0"),
-    ("R3", "Eth0/2"): ("R5", "Eth0/0"),
-    ("SW1", "Eth0/1"): ("R1", "Eth0/0"),
-    ("SW1", "Eth0/2"): ("R2", "Eth0/0"),
-    ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
-}
+
+class Topology:
+    def __init__(self, topology_dict):
+        self.topology = self._normalize(topology_dict)
+
+    def __str__(self):
+        return f"{self.topology}"
+
+    def __repr__(self):
+        return f"Topology {self}"
+
+    def _normalize(self, topology_dict):
+        d = {}
+        for key, value in topology_dict.items():
+            if key not in d.values():
+                d[key] = value
+        return d
+
+    def delete_link(self, key, value):
+        if (key in self.topology.keys()) and (value in self.topology.values()):
+            self.topology.pop(key)
+        elif (key in self.topology.values()) and (value in self.topology.keys()):
+            rev_topology = {v: k for k, v in self.topology.items()}
+            rev_topology.pop(key)
+            self.topology = {k: v for v, k in rev_topology.items()}
+        else:
+            print(f"Такого соединения нет")
+
+
+if __name__ == "__main__":
+    topology_example = {
+        ("R1", "Eth0/0"): ("SW1", "Eth0/1"),
+        ("R2", "Eth0/0"): ("SW1", "Eth0/2"),
+        ("R2", "Eth0/1"): ("SW2", "Eth0/11"),
+        ("R3", "Eth0/0"): ("SW1", "Eth0/3"),
+        ("R3", "Eth0/1"): ("R4", "Eth0/0"),
+        ("R3", "Eth0/2"): ("R5", "Eth0/0"),
+        ("SW1", "Eth0/1"): ("R1", "Eth0/0"),
+        ("SW1", "Eth0/2"): ("R2", "Eth0/0"),
+        ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
+    }
+    top = Topology(topology_example)
+    print(top.topology)
+    top.delete_link(("R1", "Eth0/0"), ("SW1", "Eth0/1"))
+    top.delete_link(("R5", "Eth0/0"), ("R3", "Eth0/2"))
+    print(top.topology)
