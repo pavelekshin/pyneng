3f7b6c92da920f3500a3ee2d9cec15528354380b
Pavel Ekshin <pavel.ekshine@gmail.com>
2022-07-15 02:27:32 -0500

    Сделаны задания 22_1c

--- exercises/22_oop_basics/task_22_1c.py
+++ exercises/22_oop_basics/task_22_1c.py
@@ -36,14 +36,70 @@ In [5]: t.delete_node('SW1')
 
 """
 
-topology_example = {
-    ("R1", "Eth0/0"): ("SW1", "Eth0/1"),
-    ("R2", "Eth0/0"): ("SW1", "Eth0/2"),
-    ("R2", "Eth0/1"): ("SW2", "Eth0/11"),
-    ("R3", "Eth0/0"): ("SW1", "Eth0/3"),
-    ("R3", "Eth0/1"): ("R4", "Eth0/0"),
-    ("R3", "Eth0/2"): ("R5", "Eth0/0"),
-    ("SW1", "Eth0/1"): ("R1", "Eth0/0"),
-    ("SW1", "Eth0/2"): ("R2", "Eth0/0"),
-    ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
-}
+
+class Topology:
+    def __init__(self, topology_dict):
+        self.topology = self._normalize(topology_dict)
+
+    def __str__(self):
+        return f"{self.topology}"
+
+    def __repr__(self):
+        return f"Topology {self}"
+
+    def __iter__(self):
+        print("iter!")
+        return iter(self.topology)
+
+    def _normalize(self, topology_dict):
+        d = {}
+        for key, value in topology_dict.items():
+            if key not in d.values():
+                d[key] = value
+        return d
+
+    def delete_link(self, key, value):
+        if self.topology.get(key) == value:
+            del self.topology[key]
+        elif self.topology.get(value) == key:
+            del self.topology[value]
+        else:
+            print(f"Такого соединения нет")
+
+    def delete_node(self, node):
+        if self._check_node_in_topology(node):
+            d = {
+                k: v
+                for k, v in self.topology.items()
+                if (node not in k) and (node not in v)
+            }
+            self.topology = d
+        else:
+            print(f"Такого устройства нет")
+
+    def _check_node_in_topology(self, node):
+        s = set()
+        for k, v in self.topology.items():
+            s.add(k[0])
+            s.add(v[0])
+        if node in s:
+            return True
+        else:
+            return False
+
+
+if __name__ == "__main__":
+    topology_example = {
+        ("R1", "Eth0/0"): ("SW1", "Eth0/1"),
+        ("R2", "Eth0/0"): ("SW1", "Eth0/2"),
+        ("R2", "Eth0/1"): ("SW2", "Eth0/11"),
+        ("R3", "Eth0/0"): ("SW1", "Eth0/3"),
+        ("R3", "Eth0/1"): ("R4", "Eth0/0"),
+        ("R3", "Eth0/2"): ("R5", "Eth0/0"),
+        ("SW1", "Eth0/1"): ("R1", "Eth0/0"),
+        ("SW1", "Eth0/2"): ("R2", "Eth0/0"),
+        ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
+    }
+    top = Topology(topology_example)
+    print(top.topology)
+    top.delete_node("SW1")
