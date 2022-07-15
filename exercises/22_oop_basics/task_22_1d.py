a21649eacff682f3b90757e31f38a5bc5ff311b5
Pavel Ekshin <pavel.ekshine@gmail.com>
2022-07-15 03:15:33 -0500

    Сделаны задания 22_1d

--- exercises/22_oop_basics/task_22_1d.py
+++ exercises/22_oop_basics/task_22_1d.py
@@ -45,14 +45,82 @@ Cоединение с одним из портов существует
 
 """
 
-topology_example = {
-    ("R1", "Eth0/0"): ("SW1", "Eth0/1"),
-    ("R2", "Eth0/0"): ("SW1", "Eth0/2"),
-    ("R2", "Eth0/1"): ("SW2", "Eth0/11"),
-    ("R3", "Eth0/0"): ("SW1", "Eth0/3"),
-    ("R3", "Eth0/1"): ("R4", "Eth0/0"),
-    ("R3", "Eth0/2"): ("R5", "Eth0/0"),
-    ("SW1", "Eth0/1"): ("R1", "Eth0/0"),
-    ("SW1", "Eth0/2"): ("R2", "Eth0/0"),
-    ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
-}
+
+class Topology:
+    def __init__(self, topology_dict):
+        self.topology = self._normalize(topology_dict)
+
+    def __str__(self):
+        return f"{self.topology}"
+
+    def __repr__(self):
+        return f"Topology {self}"
+
+    def __iter__(self):
+        print("iter!")
+        return iter(self.topology)
+
+    def _normalize(self, topology_dict):
+        d = {}
+        for key, value in topology_dict.items():
+            if key not in d.values():
+                d[key] = value
+        return d
+
+    def delete_link(self, key, value):
+        if self.topology.get(key) == value:
+            del self.topology[key]
+        elif self.topology.get(value) == key:
+            del self.topology[value]
+        else:
+            print(f"Такого соединения нет")
+
+    def delete_node(self, node):
+        if self._check_node_in_topology(node):
+            for k, v in list(self.topology.items()):
+                if (node in k) or (node in v):
+                    del self.topology[k]
+        else:
+            print(f"Такого устройства нет")
+
+    def _check_node_in_topology(self, node):
+        s = set()
+        for k, v in self.topology.items():
+            s.add(k[0])
+            s.add(v[0])
+        if node in s:
+            return True
+        else:
+            return False
+
+    def add_link(self, src, dst):
+        if (src in self.topology.keys()) and (dst in self.topology.values()):
+            print(f"Такое соединение существует")
+        elif (
+            (src in self.topology.keys())
+            or (dst in self.topology.values())
+        ):
+            self.topology[src] = dst
+            print(f"Cоединение с одним из портов существует")
+        else:
+            self.topology[src] = dst
+
+
+if __name__ == "__main__":
+    topology_example = {
+        ("R1", "Eth0/0"): ("SW1", "Eth0/1"),
+        ("R2", "Eth0/0"): ("SW1", "Eth0/2"),
+        ("R2", "Eth0/1"): ("SW2", "Eth0/11"),
+        ("R3", "Eth0/0"): ("SW1", "Eth0/3"),
+        ("R3", "Eth0/1"): ("R4", "Eth0/0"),
+        ("R3", "Eth0/2"): ("R5", "Eth0/0"),
+        ("SW1", "Eth0/1"): ("R1", "Eth0/0"),
+        ("SW1", "Eth0/2"): ("R2", "Eth0/0"),
+        ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
+    }
+    top = Topology(topology_example)
+    print(top.topology)
+    top.delete_node("SW1")
+    top.add_link(("SW1", "Eth0/1"), ("R1", "Eth0/0"))
+    top.add_link(("SW1", "Eth0/1"), ("R9", "Eth0/0"))
+    print(top.topology)
