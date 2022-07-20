adb537a439ba90e90522b8a53702fd1f5d23c3f6
Pavel Ekshin <pavel.ekshine@gmail.com>
2022-07-20 14:39:55 -0500

    Сделаны задания 22_2a

--- exercises/22_oop_basics/task_22_2a.py
+++ exercises/22_oop_basics/task_22_2a.py
@@ -50,3 +50,54 @@ up      \r\nEthernet0/1                192.168.200.1   YES NVRAM  up...'
 
 
 """
+from textfsm import clitable
+import telnetlib
+import time
+from pprint import pprint
+
+
+class CiscoTelnet:
+    def __init__(self, ip, username, password, secret):
+        self.telnet = telnetlib.Telnet(ip)
+        self.telnet.read_until(b"Username")
+        self._write_line(username)
+        self.telnet.read_until(b"Password")
+        self._write_line(password)
+        if secret:
+            self.telnet.write(b"enable\n")
+            self.telnet.read_until(b"Password")
+            self._write_line(secret)
+        time.sleep(0.5)
+        self.telnet.read_very_eager()
+
+    def _write_line(self, line):
+        self.telnet.write(line.encode("ascii") + b"\n")
+
+    def _parse_output(self, command, command_output, templates="templates", index="index"
+    ):
+        cli_table = clitable.CliTable(index, templates)
+        cli_table.ParseCmd(command_output, {"Command": command})
+        data_rows = [list(row) for row in cli_table]
+        header = list(cli_table.header)
+        return [dict(zip(header, line)) for line in data_rows]
+
+    def send_show_command(
+        self, command, parse=True, templates="templates", index="index"
+    ):
+        self._write_line(command)
+        output = self.telnet.read_until(b"#").decode("utf-8")
+        if parse:
+            return self._parse_output(command, output)
+        else:
+            return output
+
+
+if __name__ == "__main__":
+    r1_params = {
+        "ip": "192.168.100.1",
+        "username": "cisco",
+        "password": "cisco",
+        "secret": "cisco",
+    }
+    r1 = CiscoTelnet(**r1_params)
+    print(r1.send_show_command("show clock", parse=True))
