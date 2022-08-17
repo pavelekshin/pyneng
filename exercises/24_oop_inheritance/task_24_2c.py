d2cdf9de6fc43167b9ffdc3fdffd5e7e060199d2
Pavel Ekshin <pavel.ekshine@gmail.com>
2022-08-17 04:59:39 -0500

    Сделаны задания 24_2c

--- exercises/24_oop_inheritance/task_24_2c.py
+++ exercises/24_oop_inheritance/task_24_2c.py
@@ -22,3 +22,62 @@ In [5]: r1.send_command('sh ip int br', strip_command=True)
 Out[5]: 'Interface                  IP-Address      OK? Method Status                Protocol\nEthernet0/0                192.168.100.1   YES NVRAM  up                    up      \nEthernet0/1                192.168.200.1   YES NVRAM  up                    up      \nEthernet0/2                190.16.200.1    YES NVRAM  up                    up      \nEthernet0/3                192.168.230.1   YES NVRAM  up                    up      \nEthernet0/3.100            10.100.0.1      YES NVRAM  up                    up      \nEthernet0/3.200            10.200.0.1      YES NVRAM  up                    up      \nEthernet0/3.300            10.30.0.1       YES NVRAM  up                    up      '
 
 """
+
+import re
+from netmiko.cisco.cisco_ios import CiscoIosSSH
+from pprint import pprint
+
+class ErrorInCommand(Exception):
+    """
+    Исключение генерируется, если при выполнении команды на оборудовании,
+    возникла ошибка.
+    """
+
+
+class MyNetmiko(CiscoIosSSH):
+    def __init__(self, **device_params):
+        super().__init__(**device_params)
+        self.enable()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.disconnect()
+
+    def _check_error_in_command(self, command, output):
+        regex = "% (?P<err>.+)"
+        match = re.search(regex, output)
+        if match:
+            raise ErrorInCommand(
+                f"""Комманда "{command}" выполнилась с ошибкой "{match.group("err")}" на устройстве {self.host}"""
+            )
+        else:
+            return False
+
+    def send_command(self, command, **kwargs):
+        output = super().send_command(command, **kwargs)
+        if not self._check_error_in_command(command, output):
+            return output
+
+    def send_config_set(self, commands):
+        if isinstance(commands, str):
+            commands = [commands]
+        result = ""
+        for command in commands:
+            output = super().send_config_set(command)
+            if not self._check_error_in_command(command, output):
+                result += output
+
+
+if __name__ == "__main__":
+    device_params = {
+        "device_type": "cisco_ios",
+        "ip": "192.168.100.1",
+        "username": "cisco",
+        "password": "cisco",
+        "secret": "cisco",
+    }
+    with MyNetmiko(**device_params) as r1:
+        pprint(r1.send_command("sh ip int br", strip_command=False))
+        pprint(r1.send_command("sh ip int br", strip_command=True))
