02438315c640558e7e27aa0de0207571513b326b
Pavel Ekshin <pavel.ekshine@gmail.com>
2022-08-17 04:39:31 -0500

    Сделаны задания 24_2b

--- exercises/24_oop_inheritance/task_24_2b.py
+++ exercises/24_oop_inheritance/task_24_2b.py
@@ -26,3 +26,62 @@ ErrorInCommand                            Traceback (most recent call last)
 ErrorInCommand: При выполнении команды "lo" на устройстве 192.168.100.1 возникла ошибка "Incomplete command."
 
 """
+import re
+from netmiko.cisco.cisco_ios import CiscoIosSSH
+
+
+class ErrorInCommand(Exception):
+    """
+    Исключение генерируется, если при выполнении команды на оборудовании,
+    возникла ошибка.
+    """
+
+
+class MyNetmiko(CiscoIosSSH):
+    def __init__(self, **device_params):
+        super().__init__(**device_params)
+        self.enable()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.disconnect()
+
+    def _check_error_in_command(self, command, output):
+        regex = r"Invalid input detected" r"|Incomplete command" r"|Ambiguous command"
+        match = re.search(regex, output)
+        if match:
+            raise ErrorInCommand(
+                f"""Комманда "{command}" выполнилась с ошибкой "{match.group()}" на устройстве {self.host}"""
+            )
+        else:
+            return False
+
+    def send_command(self, command):
+        output = super().send_command(command)
+        if not self._check_error_in_command(command, output):
+            return output
+
+    def send_config_set(self, commands):
+        if commands is list:
+            for command in commands:
+                output = super().send_config_set(command)
+                if not self._check_error_in_command(command, output):
+                    result = +output
+        else:
+            output = super().send_config_set(commands)
+            if not self._check_error_in_command(commands, output):
+                result = +output
+
+
+if __name__ == "__main__":
+    device_params = {
+        "device_type": "cisco_ios",
+        "ip": "192.168.100.1",
+        "username": "cisco",
+        "password": "cisco",
+        "secret": "cisco",
+    }
+    with MyNetmiko(**device_params) as r1:
+        print(r1.send_config_set("sh ip int biasfr"))
